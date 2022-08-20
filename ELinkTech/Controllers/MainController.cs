2decc41a49b3e23c7fef4d17eeef7f910aaf8eed
FinnMac <32690795+FinnMac@users.noreply.github.com>
2022-08-20 16:03:43 +1000

    Initial commit
    initial commitment to the project.

---
+++ ELinkTech/Controllers/MainController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ELinkTech.Models;
+
+namespace ELinkTech.Controllers;
+
+public class MainController : Controller
+{
+    private readonly ILogger<MainController> _logger;
+
+    public MainController(ILogger<MainController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
