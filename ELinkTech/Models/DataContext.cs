0dcab2c5e817fbe6a59e55bd0f23a77d280e9cd8
samdelooze <samstruk7822@gmail.com>
2022-08-24 14:30:29 +1000

    created the initial database

---
+++ ELinkTech/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+
+namespace ELinkTech.Models
+{
+    public class DataContext:IdentityDbContext<ApplicationUser>
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- ELinkTech/Program.cs
+++ ELinkTech/Program.cs
@@ -1,6 +1,20 @@
+using ELinkTech.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var conString = builder.Configuration.GetConnectionString("ConnectionString");
 // Add services to the container.
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlite(conString));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<DataContext>()
+    .AddDefaultTokenProviders()
+    .AddRoles<IdentityRole>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
