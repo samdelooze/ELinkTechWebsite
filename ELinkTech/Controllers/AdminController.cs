3aa47c698a42af821dcf806566f36f8d3cbac78c
samdelooze <samstruk7822@gmail.com>
2022-10-26 09:47:28 +1100

    role management

---
+++ ELinkTech/Controllers/AdminController.cs
@@ -0,0 +1,43 @@
+﻿using ELinkTech.Models;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ELinkTech.Controllers
+{
+    public class AdminController : Controller
+    {
+        private UserManager<ApplicationUser> userManager { get; }
+        private SignInManager<ApplicationUser> signInManager { get; }
+        private readonly DataContext db;
+
+        public AdminController(UserManager<ApplicationUser> userManager,
+                SignInManager<ApplicationUser> signInManager,
+                DataContext db)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.db = db;
+        }
+        [HttpGet]
+        public IActionResult GetCategories()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult EditCategory()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult DeleteCategory()
+        {
+            return View();
+        }
+    }
+}
---
+++ ELinkTech/SeedData.cs
@@ -0,0 +1,46 @@
+﻿using ELinkTech.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ELinkTech
+{
+    public class SeedData
+    {
+        public static void Seed(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            SeedRoles(roleManager);
+            SeedUsersAsync(userManager);
+        }
+
+        private static async Task SeedUsersAsync(UserManager<ApplicationUser> userManager)
+        {
+            if (userManager.FindByNameAsync("Admin").Result == null)
+            {
+                var user = new ApplicationUser { UserName = "Admin", Email = "Admin@admin" };
+                var result = userManager.CreateAsync(user, "Password123!").Result;
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Administrator").Wait();
+                    var token = await userManager.GenerateEmailConfirmationTokenAsync(user);
+                    var confirmed = userManager.ConfirmEmailAsync(user, token);
+                }
+            }
+        }
+        private static void SeedRoles(RoleManager<IdentityRole> roleManager)
+        {
+            if (!roleManager.RoleExistsAsync("Administrator").Result)
+            {
+                var role = new IdentityRole { Name = "Administrator" };
+                var result = roleManager.CreateAsync(role).Result;
+            }
+            if (!roleManager.RoleExistsAsync("User").Result)
+            {
+                var role = new IdentityRole { Name = "User" };
+                var result = roleManager.CreateAsync(role).Result;
+            }
+        }
+    }
+}
