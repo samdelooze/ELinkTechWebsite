9e4cea3357a935d439b200597d9e46908f75a439
Sam De Looze <95002159+samdelooze@users.noreply.github.com>
2022-10-27 11:25:30 +1100

    Update Main.cs
    Because we have one model shared between many views, we can't really use [Required]. Since the register is now its own view, we can make a new ViewModel for the register.

--- ELinkTech/ViewModels/Main.cs
+++ ELinkTech/ViewModels/Main.cs
@@ -19,52 +19,41 @@ namespace ELinkTech.ViewModels
         public bool RememberMe { get; set; }
 
         //REGISTER
-        [Required]
         [DataType(DataType.EmailAddress)]
         [Display(Name = "Email Address")]
         public string? RegisterEmail { get; set; }
 
-        [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string? RegisterPassword { get; set; }
 
-        [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Confirm Password")]
         [Compare(nameof(RegisterPassword), ErrorMessage = "Passwords do not match")]
         public string? ConfirmPassword { get; set; }
 
-        [Required]
         [Display(Name = "First Name")]
         public string? FirstName { get; set; }
 
-        [Required]
         [Display(Name = "Last Name")]
         public string? LastName { get; set; }
 
-        [Required]
         [Phone]
         [Display(Name = "Contact Number")]
         public string? ContactNumber { get; set; }
 
-        [Required]
         [Display(Name = "State")]
         public string? State { get; set; }
 
-        [Required]
         [Display(Name = "Address")]
         public string? Address { get; set; }
 
-        [Required]
         [Display(Name = "Suburb")]
         public string? Suburb { get; set; }
 
-        [Required]
         [Display(Name = "Postcode")]
         public string? Postcode { get; set; }
 
-        [Required]
         [Display(Name = "Date of Birth")]
         public DateTime? DateOfBirth { get; set; }
     }
